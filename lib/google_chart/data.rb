09516b607246107112d2a2383800bfdec9ceac26
John Parker <jparker@urgetopunt.com>
2008-05-13 14:20:03 -0700

    Support for text encoding, support for automatic scaling of input data.

--- lib/google_chart/data.rb
+++ lib/google_chart/data.rb
@@ -1,6 +1,6 @@
 module GoogleChart
   module Data
-    attr_writer :encoding
+    attr_writer :encoding, :scale
     
     @@simple_encoding   = ('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a
     @@extended_alphabet = ('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a + %w[- .]
@@ -12,15 +12,19 @@ module GoogleChart
       'chd=' + send(:"#{encoding}_encode", @data) if @data
     end
     
-    def encoding
-      @encoding || :simple
-    end
-    
     def data=(data)
       @data = data.any? {|e| e.is_a?(Array) } ? data : [data]
     end
     
     private
+    def encoding
+      @encoding || :simple
+    end
+    
+    def scale?
+      @scale
+    end
+    
     def simple_encoding
       @@simple_encoding
     end
@@ -29,14 +33,27 @@ module GoogleChart
       @@extended_encoding
     end
     
+    def scale(set, range)
+      if scale?
+        min, max = set.min, set.max
+        if min < 0
+          set.collect! {|n| n - min }
+          max -= min
+        end
+        set.collect {|n| (n * range).to_f / max if n }
+      else
+        set
+      end
+    end
+    
     def simple_encode(data)
       's:' + data.collect {|set|
-        set.collect {|n|
+        scale(set, @@simple_encoding.size - 1).collect {|n|
           case
-          when n.nil?                     : '_'
-          when n <= 0                     : @@simple_encoding[0]
-          when n >= @@simple_encoding.size: @@simple_encoding[-1]
-          else                              @@simple_encoding[n]
+          when n.nil? then '_'
+          when n <= 0 then @@simple_encoding[0]
+          when n >= @@simple_encoding.size then @@simple_encoding[-1]
+          else @@simple_encoding[n.round]
           end
         }.join
       }.join(',')
@@ -44,15 +61,34 @@ module GoogleChart
     
     def extended_encode(data)
       'e:' + data.collect {|set|
-        set.collect {|n|
+        scale(set, @@extended_encoding.size - 1).collect {|n|
           case
-          when n.nil?: '__'
-          when n <= 0: @@extended_encoding[0]
-          when n >= @@extended_encoding.size: @@extended_encoding[-1]
-          else @@extended_encoding[n]
+          when n.nil? then '__'
+          when n <= 0 then @@extended_encoding[0]
+          when n >= @@extended_encoding.size then @@extended_encoding[-1]
+          else @@extended_encoding[n.round]
           end
         }.join
       }.join(',')
+    end
+    
+    def text_encode(data)
+      't:' + data.collect {|set|
+        scale(set, 100).collect {|n|
+          case
+          when n.nil? then -1
+          when n < 0 then 0
+          when n > 100 then 100
+          else
+            # More than one decimal place yields negligible resolution at the
+            # potentially great cost of one extra character in the URL. Avoid
+            # this by rounding to the nearest tenth. Further round down to an
+            # integer when possible to save the extra two characters (".0").
+            m = (n * 10).round
+            m / (m % 10 == 0 ? 10 : 10.0)
+          end
+        }.join(',')
+      }.join('|')
     end
   end
 end
--- test/google_chart/test_data.rb
+++ test/google_chart/test_data.rb
@@ -9,6 +9,10 @@ class TestData < Test::Unit::TestCase
     assert_equal('chd=s:AB89', @klass.new(:data => [0,1,60,61]).data)
   end
   
+  def test_should_be_able_to_scale_over_wide_data_sets_to_fit_within_simple_encoding_range
+    assert_equal('chd=s:Af9', @klass.new(:data => [-100, 0, 100], :scale => true).data)
+  end
+  
   def test_should_be_able_to_use_simple_encoding_on_multiple_data_sets
     assert_equal('chd=s:JP,YH', @klass.new(:data => [[9,15], [24,7]]).data)
   end
@@ -17,11 +21,43 @@ class TestData < Test::Unit::TestCase
     assert_equal('chd=s:y_h', @klass.new(:data => [50,nil,33]).data)
   end
   
+  def test_should_correctly_encode_floating_point_data_using_simple_encoding
+    assert_equal('chd=s:yh', @klass.new(:data => [50.49, 32.51]).data)
+  end
+  
   def test_should_be_able_to_use_extended_encoding
     assert_equal('chd=e:jpyh,JPYH', @klass.new(:data => [[2281,3233], [591,1543]], :encoding => :extended).data)
   end
   
-  def test_should_correnctly_encode_missing_data_using_extended_encoding
+  def test_should_be_able_to_scale_over_wide_data_sets_to_fit_within_extended_encoding_range
+    assert_equal('chd=e:AAgA..', @klass.new(:data => [-5000, 0, 5000], :encoding => :extended, :scale => true).data)
+  end
+  
+  def test_should_correctly_encode_missing_data_using_extended_encoding
     assert_equal('chd=e:jp__yh', @klass.new(:data => [2281,nil,3233], :encoding => :extended).data)
+  end
+  
+  def test_should_correctly_encode_floating_point_data_using_extended_encoding
+    assert_equal('chd=e:jpyh', @klass.new(:data => [2281.49, 3232.51], :encoding => :extended).data)
+  end
+  
+  def test_should_be_able_to_use_text_encoding
+    assert_equal('chd=t:6,14,19.5', @klass.new(:data => [6, 14, 19.5], :encoding => :text).data)
+  end
+  
+  def test_should_be_able_to_scale_over_wide_data_sets_to_fit_within_text_encoding_range
+    assert_equal('chd=t:0,50,100', @klass.new(:data => [-200, 0, 200], :encoding => :text, :scale => true).data)
+  end
+  
+  def test_should_correctly_encode_missing_data_using_text_encoding
+    assert_equal('chd=t:6,-1,14', @klass.new(:data => [6, nil, 14], :encoding => :text).data)
+  end
+  
+  def test_should_round_floating_point_data_to_one_decimal_place_using_text_encoding
+    assert_equal('chd=t:6.1,8.2', @klass.new(:data => [6.14, 8.16], :encoding => :text).data)
+  end
+  
+  def test_should_round_floating_point_data_to_integer_data_when_possible_using_text_encoding
+    assert_equal('chd=t:6,14', @klass.new(:data => [5.95, 14.01], :encoding => :text).data)
   end
 end
