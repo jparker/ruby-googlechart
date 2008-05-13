90918f17733bb1484c25f6f435d60ecf76bb4830
John Parker <jparker@urgetopunt.com>
2008-05-13 14:57:45 -0700

    Added support for range markers

---
+++ lib/google_chart/range_markers.rb
@@ -0,0 +1,21 @@
+module GoogleChart
+  module RangeMarkers
+    @@range_marker_orientations = { :h => 'r', :v => 'R' }
+    
+    def ranges
+      unless @ranges.nil? || @ranges.empty?
+        'chm=' + @ranges.collect {|r|
+          r[0] = @@range_marker_orientations[r[0]]
+          r[1..-1] = ['000000cc', *r[1..-1]] if r.size == 3
+          # The middle parameter is required by Google Charts but ignored for range markers
+          r = r[0..1] + ['x'] + r[2..-1]
+          r.join(',')
+        }.join('|')
+      end
+    end
+    
+    def ranges=(ranges)
+      @ranges = ranges.any? {|e| e.is_a?(Array) } ? ranges : [ranges]
+    end
+  end
+end
