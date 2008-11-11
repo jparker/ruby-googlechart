d08f895c433a33c25d97265a54cc5a61c6fa437c
John Parker <jparker@urgetopunt.com>
2008-11-10 18:48:30 -0800

    Rewrite in progress

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-unless ENV['TM_MODE']
-  require 'rubygems'
-  require 'redgreen'
-end
-require File.dirname(__FILE__) + '/../lib/google_chart'
-
-class TestChart < GoogleChart::Base
-end
